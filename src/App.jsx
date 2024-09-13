22a5531f44d80f344fa41eb1da90916033ad55be
lirajoaop <joaopedroababa132@gmail.com>
2024-09-12 23:13:34 -0300

    Add ReactSwitch and implement theme toggling using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import ReactSwitch from "react-switch";
 import { Form } from "./components/Form";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", theme === "dark");
+  }, [theme]);
+
   return (
-    <div className="w-screen h-screen flex justify-center items-center flex-col bg-[#f3ebf6] dark:bg-[#13111b]  ">
+    <div
+      className="w-screen h-screen flex justify-center items-center flex-col bg-[#f3ebf6] dark:bg-[#13111b]"
+      id="theme"
+    >
       <Form />
+      <br />
+      <br />
+      <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
     </div>
   );
 }
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,28 +1,33 @@
 export const Form = () => {
   return (
     <div className="w-[400px] h-[400px] border rounded-[1.5rem] shadow-custom bg-white dark:bg-[#201b2d]">
-      <p className="pt-10 text-[23px] font-[700] text-[#5a4b81] flex justify-center items-center flex-col dark:text-white">
+      <p className="pt-10 text-[23px] font-bold text-[#5a4b81] flex justify-center items-center flex-col dark:text-white">
         Jovem Programador
       </p>
 
       <form className="flex justify-center items-center flex-col">
         <input
           type="text"
-          className="w-[76%] font-[700] text-[14px] tracking-[1px] p-[10px] px-[20px] border-2 border-custom-gray rounded-[20px] outline-none box-border mb-[50px] mt-[50px] text-center font-ubuntu text-[rgb(38,50,56)] bg-dark-gray dark:text-[rgb(254,255,255)] dark:bg-soft-pink dark:placeholder:text-[rgb(254,255,255)] focus:border-light-black focus:border-2"
+          className="w-[76%] font-bold text-[14px] tracking-[1px] p-[10px] px-[20px] border-2 border-custom-gray rounded-[20px] outline-none box-border mb-[50px] mt-[50px] text-center font-ubuntu text-[rgb(38,50,56)] bg-dark-gray dark:text-[rgb(254,255,255)] dark:bg-soft-pink dark:placeholder:text-[rgb(254,255,255)] focus:border-light-black focus:border-2"
           placeholder="Usuário"
         />
         <input
           type="password"
-          className="w-[76%] font-[700] text-[14px] tracking-[1px] p-[10px] px-[20px] border-2 border-custom-gray rounded-[20px] outline-none box-border mb-[50px] text-center font-ubuntu text-[rgb(38,50,56)] bg-dark-gray dark:text-[rgb(254,255,255)]  dark:bg-soft-pink dark:placeholder:text-[rgb(254,255,255)]"
+          className="w-[76%] font-bold text-[14px] tracking-[1px] p-[10px] px-[20px] border-2 border-custom-gray rounded-[20px] outline-none box-border mb-[50px] text-center font-ubuntu text-[rgb(38,50,56)] bg-dark-gray dark:text-[rgb(254,255,255)]  dark:bg-soft-pink dark:placeholder:text-[rgb(254,255,255)] focus:border-light-black focus:border-2"
           placeholder="Senha"
         />
 
-        <a href="#" className="[submit]">
+        <a
+          href="#"
+          className="cursor-pointer rounded-[5rem] text-white bg-[#5a4b81] w-[76%] h-[40px] flex items-center justify-center no-underline dark:bg-[#3e335c]"
+        >
           Entrar
         </a>
 
-        <p className="[forgot]">
-          <a href="#">Esqueceu a senha?</a>
+        <p className="text-[#5a4b81] dark:text-[#9f91c5] text-[14px] font-bold tracking-[1px] mt-3 ">
+          <a className="" href="#">
+            Esqueceu a senha?
+          </a>
         </p>
       </form>
     </div>
